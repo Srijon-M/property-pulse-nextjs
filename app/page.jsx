007bfc1335e5f9b07c1fa212966e42b8666814e3
Srijon-M <connectwithsriji@gmail.com>
2024-10-08 10:42:47 +0530

    hero section footer and info boxes

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
     return(
-        <div>
-            <h1 className="text-3xl">Welcome to Pulse Property</h1>
-            <Link href='/properties'>Go To Properties</Link>
-        </div>
+        <>
+            <Hero/>
+            <InfoBoxes/>
+        </>
     );
 };
 
